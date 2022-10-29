bd74cdb41452ffb15432e96d0c514fbd26d900eb
Piscean <conanchihieu@gmail.com>
2022-10-29 15:07:56 +0700

    Add HTTPS support

--- app/Repository/FunctionRepos.php
+++ app/Repository/FunctionRepos.php
@@ -9,8 +9,8 @@ class FunctionRepos
     public static function searchForProducts($query) {
         $sql = 'select p.* ';
         $sql .= 'from product as p ';
-        $sql .= 'where  LOCATE(?, name_p) > 0 ';
-        return DB::select($sql, [$query]);
+        $sql .= 'where LOCATE(?, name_p) > 0 ';
+        return DB::select($sql, $query);
     }
     public static function getProductsByCateId($id_c){
             $sql = 'select p.* ';
