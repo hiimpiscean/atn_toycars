e1185149bb58c914ea7763e8f79fb00324f93535
Piscean <conanchihieu@gmail.com>
2022-10-30 12:47:04 +0700

    Add HTTPS support

--- resources/views/product/confirm.blade.php
+++ resources/views/product/confirm.blade.php
@@ -23,13 +23,13 @@
             <h1 class="display-4 text-center">Are you sure you want to delete?</h1>
         </div>
         @include('product.productDetails')
-        <form action="{{route('handicraft.destroy', ['id_p' =>$product->id_p])}}" method="post">
+        <form action="{{route('product.destroy', ['id_p' =>$product->id_p])}}" method="post">
             @csrf
             <input type="hidden" name="id_p" value="{{$product->id_p}}">
 
             <button type="submit" class="btn btn-danger but">Delete</button>
 
-            <a href="{{route('handicraft.index')}}" class="btn btn-info cancel">Cancel</a>
+            <a href="{{route('product.index')}}" class="btn btn-info cancel">Cancel</a>
         </form>
     </div>
 @endsection
